07c8f0ff2f64b59d1f083b46fe061ea1e3f64417
timilehin2000 <joeltimmy7@gmail.com>
2024-03-10 21:08:22 +0100

    chore: added post method to add book

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Learning to work with Golang API on the backend
 
-This project is meant to help me learn how to use Golang + MYSql + Kubernetes by building a simple REST API. I plan to document my journey here to help me stay consistent and for accountability
+This project is meant to help me learn how to use Golang + MySql + Kubernetes by building a simple REST API. I plan to document my journey here to help me stay consistent and for accountability sake.
 
 DAY 1:
 
-Learnt how to set up a server, how to use different packages to serve different functions e.g mux, mysqldriver. Also learnt how to create a mysql connection, how to query from the DB using go's mysql driver.
+Learnt how to set up a server, how to use different packages to serve different functions e.g mux, mysqldriver. Also learnt how to create mysql connection, how to query from the DB using go's mysql driver.
+
+DAY 2:
+
+Learnt how to write a method to add a book instance to the DB.
---
+++ pkg/main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+type library struct {
+	dbHost, dbPassword, dbName string
+}
+
+type Book struct {
+	Id   string
+	Name string
+	ISBN string
+}
+
+const (
+	ApiPath = "/api/v1/books"
+)
+
+func main() {
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		dbHost = "localhost:3306"
+	}
+
+	dbPassword := os.Getenv("DB_PASSWORD")
+	if dbPassword == "" {
+		dbPassword = "timmy419"
+	}
+
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		dbName = "library"
+	}
+
+	apiPath := os.Getenv("API_PATH")
+	if apiPath == "" {
+		apiPath = ApiPath
+	}
+
+	l := library{
+		dbHost:     dbHost,
+		dbName:     dbName,
+		dbPassword: dbPassword,
+	}
+
+	l.openConnection()
+
+	r := mux.NewRouter()
+	r.HandleFunc(apiPath, l.GetBooks).Methods("GET")
+	r.HandleFunc(apiPath, l.AddBook).Methods("POST")
+
+	fmt.Println("Server is running")
+	http.ListenAndServe(":8080", r)
+}
+
+func (l library) AddBook(w http.ResponseWriter, r *http.Request) {
+	book := Book{}
+	json.NewDecoder(r.Body).Decode(&book)
+
+	db := l.openConnection()
+
+	insertQuery, err := db.Prepare("insert into books values (?, ?, ?)")
+	if err != nil {
+		log.Fatalf("preparing the db query %s\n", err.Error())
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatalf("While beginning the transaction %s\n", err.Error())
+	}
+
+	_, err = tx.Stmt(insertQuery).Exec(book.Id, book.Name, book.ISBN)
+	if err != nil {
+		log.Fatalf("")
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatalf("While commit the transaction %s\n", err.Error())
+	}
+
+	l.closeConnection(db)
+}
+
+func (l library) GetBooks(w http.ResponseWriter, r *http.Request) {
+	db := l.openConnection()
+
+	rows, err := db.Query("SELECT * FROM books")
+
+	if err != nil {
+		log.Fatalf("Error loading books %s\n", err.Error())
+	}
+
+	books := []Book{}
+
+	for rows.Next() {
+		var id, name, isbn string
+		err := rows.Scan(&id, &name, &isbn)
+
+		if err != nil {
+			log.Fatalf("Scanning the rows %s\n", err.Error())
+		}
+
+		book := Book{
+			Id:   id,
+			Name: name,
+			ISBN: isbn,
+		}
+
+		books = append(books, book)
+
+	}
+
+	json.NewEncoder(w).Encode(books)
+
+	l.closeConnection(db)
+
+}
+
+func (l library) openConnection() *sql.DB {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", "root", l.dbPassword, l.dbHost, l.dbName))
+	if err != nil {
+		log.Fatalf("Cannot open DB connection %s\n", err.Error())
+	}
+
+	return db
+}
+
+func (l library) closeConnection(db *sql.DB) {
+	err := db.Close()
+
+	if err != nil {
+		log.Fatalf("Closing DB %s/n", err.Error())
+	}
+}
